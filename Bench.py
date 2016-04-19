b9af0304a00c56946df6af32a88a652780d9e39d
OGSocks <kmarshall5696@gmail.com>
2016-04-19 15:44:31 -0400

    Update Bench.py

--- Bench.py
+++ Bench.py
@@ -3,6 +3,7 @@ import time
 import math
 import Adafruit_CharLCDPlate
 
+userFile = ""
 lcd = display.lcd
 trig0 = OutputDevice()
 echo0= InputDevice()
@@ -16,6 +17,15 @@ lastTime = time()
 weightFlag = False
 
 sleep(2)
+
+def get_Users():
+    users = {}
+    f = open(userFile)
+    for line in f:
+        line = line.strip()
+        line = line.split()
+        users[line[0]] = line[1]
+    return users
 
 def get_pulse_time():
     trig.on()
@@ -36,6 +46,13 @@ def calculate_distance(duration):
     speed = 343
     distance = speed * duration / 2
     return distance
+
+def calculate_velocity(d0,d1,old_d0,old_d1,last_read):
+    v0 = (d0-old_d0)/(last_read-lastTime)
+    v1 = (d1-old_d1)/(last_read-lastTime)
+    return (v0+v1)/2
+    
+
 
 while True:
     lcd.clear()
@@ -67,12 +84,16 @@ while True:
     dist1_orig = calculate_distance(duration)
     last_dist0 = dist0_orig
     last_dist1 = dist1_orig
+    lastTime = time()
+    v_last = 0
     lift_error = 0
     while True:
         duration = get_pulse_time()
         dist0 = calculate_distance(duration)
         duration = get_pulse_time()
         dist1 = calculate_distance(duration)
+        now = time()
+        v_now = calculate_velocity(dist0,dist1,last_dist0,last_dist1,now)
         if abs(dist0 - dist1) > .1:
             lift_bars()
             lift_error = 1
